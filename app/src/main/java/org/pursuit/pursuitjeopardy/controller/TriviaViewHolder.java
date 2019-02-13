e1ff1433497d53d2cf55af5bfd401d5945455245
garganjohn <garganjohn@gmail.com>
2019-02-13 16:39:45 -0500

    added stuff we need for recyclerview and json

---
+++ app/src/main/java/org/pursuit/pursuitjeopardy/controller/TriviaViewHolder.java
@@ -0,0 +1,12 @@
+package org.pursuit.pursuitjeopardy.controller;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+public class TriviaViewHolder extends RecyclerView.ViewHolder {
+    public TriviaViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+    public void onBind(){}
+}
---
+++ app/src/main/java/org/pursuit/pursuitjeopardy/network/TriviaService.java
@@ -0,0 +1,9 @@
+package org.pursuit.pursuitjeopardy.network;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface TriviaService {
+    @GET("api_config.php")
+    Call<>getTrivia();
+}
