adfa0f9ce97ea51b1f3495a529e12819831653e4
Geo Jimenez <ger.jimenez1@gmail.com>
2019-02-25 22:21:27 -0500

    implement log

--- app/src/main/java/org/pursuit/pursuitjeopardy/MainActivity.java
+++ app/src/main/java/org/pursuit/pursuitjeopardy/MainActivity.java
@@ -4,6 +4,7 @@ package org.pursuit.pursuitjeopardy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("onCreate", "created!" );
     }
 }
