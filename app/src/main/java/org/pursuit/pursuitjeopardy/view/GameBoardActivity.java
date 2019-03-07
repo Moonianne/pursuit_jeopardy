e358a958667f5ca60ec98be5df718409f8dbd75f
Eric Diaz <ericdiaz@Erics-MacBook-Pro-2.local>
2019-03-07 13:06:15 -0500

    merged with master and handled conflicts locally

--- app/src/main/java/org/pursuit/pursuitjeopardy/view/GameBoardActivity.java
+++ app/src/main/java/org/pursuit/pursuitjeopardy/view/GameBoardActivity.java
@@ -37,24 +37,6 @@ public final class GameBoardActivity extends AppCompatActivity implements OnFrag
         setDrawables();
     }
 
-    private void setClipChildren() {
-        final ViewGroup viewGroup = (ViewGroup) ((ViewGroup) this
-                .findViewById(android.R.id.content)).getChildAt(0);
-        viewGroup.setClipChildren(false);
-    }
-
-
-
-    private void setScoreBoard(BoardInflater boardInflater) {
-        scoreKeeper = boardInflater.new ScoreKeeper(scoreBoard);
-        scoreKeeper.setScoreBoard();
-    }
-
-    private void updatePlayerPoints(boolean isCorrect, String QuestionKey) {
-        viewModel.addToPlayerScore(
-                viewModel.pointsAllocator(isCorrect, viewModel.qetQuestionDifficulty(QuestionKey)));
-    }
-
     private void findAndLoadLayout() {
         scoreBoard = findViewById(R.id.score_board);
         LinearLayout category1 = findViewById(R.id.category1);
@@ -70,6 +52,22 @@ public final class GameBoardActivity extends AppCompatActivity implements OnFrag
         layoutList.add(category5);
     }
 
+    private void setClipChildren() {
+        final ViewGroup viewGroup = (ViewGroup) ((ViewGroup) this
+                .findViewById(android.R.id.content)).getChildAt(0);
+        viewGroup.setClipChildren(false);
+    }
+
+
+    private void setScoreBoard(BoardInflater boardInflater) {
+        scoreKeeper = boardInflater.new ScoreKeeper(scoreBoard);
+        scoreKeeper.setScoreBoard();
+    }
+
+    private void updatePlayerPoints(boolean isCorrect, String QuestionKey) {
+        viewModel.addToPlayerScore(
+                viewModel.pointsAllocator(isCorrect, viewModel.qetQuestionDifficulty(QuestionKey)));
+    }
 
     private void setViewModel() {
         viewModel = ViewModelProviders.of(this).get(QuestionViewModel.class);
