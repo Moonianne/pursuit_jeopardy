b4822092014352d6bbfd86db46b3d25f87640be0
Geo Jimenez <ger.jimenez1@gmail.com>
2019-03-13 10:52:10 -0400

    localize fragment tags to interface

--- app/src/main/java/org/pursuit/pursuitjeopardy/controller/OnFragmentInteractionListener.java
+++ app/src/main/java/org/pursuit/pursuitjeopardy/controller/OnFragmentInteractionListener.java
@@ -3,6 +3,9 @@ package org.pursuit.pursuitjeopardy.controller;
 import android.view.View;
 
 public interface OnFragmentInteractionListener {
+    String QUESTION_FRAGMENT_TAG = "question";
+    String RESULT_FRAGMENT_TAG = "result";
+
     void displayQuestion(String key);
 
     void displayResult(boolean isCorrect);
